14c1768770555c8991c4a8f02e253a5597fb083d
JB Larson <JB@hirejb.me>
2025-06-04 16:53:55 -0700

    Create token-config.js

---
+++ token-config.js
@@ -0,0 +1,8 @@
+// token-config.js
+// Store your GitHub Personal Access Token here.
+//
+// !!! IMPORTANT SECURITY WARNING !!!
+// This file contains your GitHub Personal Access Token in plain text.
+//
+
+const GITHUB_APP_TOKEN = "insert-read-only-github-personal-access-token-here";
